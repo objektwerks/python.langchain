d793e9386aadf11de30c3e28149799dd0ca428f6
objektwerks <objektwerks@runbox.com>
2025-06-17 07:11:11 -0400

    added imports to main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from langchain import OpenAI
+from langchain.chains import LLMChain
+
 import os
 
 def main():
     apiKey = os.getenv('OPENAI_API_KEY')
     print(f"OpenAI api key: {apiKey}")
+
+
 
 if __name__ == "__main__":
     main()
