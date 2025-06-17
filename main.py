5cfb80d55bc5fc37a062f5ee97b27e0b4b65230a
objektwerks <objektwerks@runbox.com>
2025-06-17 07:25:22 -0400

    enhanced main debug

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ import os
 
 def main():
     apiKey = os.getenv("OPENAI_API_KEY")
-    print(f"OpenAI api key: {apiKey}")
+    print(f"*** OpenAI api key: {apiKey}")
 
     modelName = "text-davinci-003"
-    print(f"OpenAI model name: {modelName}")
+    print(f"*** OpenAI model name: {modelName}")
 
     llm = OpenAI(model_name=modelName)
 
     prompt = "Write a story about a pizza."
-    print(f"Request: {prompt}")
+    print(f"*** Request: {prompt}")
 
     chain = LLMChain(llm=llm, prompt=prompt)
 
     response = chain.run()
-    print(f"Response: {response}")
+    print(f"*** Response: {response}")
 
 
 if __name__ == "__main__":
