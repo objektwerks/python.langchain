1430d6fdb31459de3522af7a86b124715ccd9be4
objektwerks <objektwerks@runbox.com>
2025-06-17 07:19:25 -0400

    formatted main

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from langchain.chains import LLMChain
 
 import os
 
+
 def main():
-    apiKey = os.getenv('OPENAI_API_KEY')
+    apiKey = os.getenv("OPENAI_API_KEY")
     print(f"OpenAI api key: {apiKey}")
 
     modelName = "text-davinci-003"
@@ -15,11 +16,11 @@ def main():
     prompt = "Write a story about a pizza."
     print(f"Request: {prompt}")
 
-
     chain = LLMChain(llm=llm, prompt=prompt)
 
     response = chain.run()
     print(f"Response: {response}")
+
 
 if __name__ == "__main__":
     main()
