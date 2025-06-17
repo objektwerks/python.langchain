552504818ded3d8f818d0b07cec9c6d61e5e98ab
objektwerks <objektwerks@runbox.com>
2025-06-17 08:44:10 -0400

    edited imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from langchain_core.runnables import RunnableSerializable
-from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSerializable
+from langchain_openai import ChatOpenAI
 
 import os
 
